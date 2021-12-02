4b7e3c763607668de9fbfa0ef8a24e8339d000a1
n4krug <gustav.eneberg@gmail.com>
2021-12-02 15:59:31 +0100

    added optipon for multiple color schemes

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "loading") {
-        if (tab.url.startsWith("https://www.vklass.se/")) {
-            chrome.tabs.insertCSS(tabId, {
-                file: "css/main.css"
-            }, () => {
-                console.log("Inserted");
-            })
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    // tabId = tabs[0].id
+    // changeInfo = tabs[0].changeInfo
+    // tab = tabs[0]
+    console.log("running")
+    chrome.storage.sync.get("colorScheme", ({colorScheme}) => {
+        console.log(colorScheme)
+        if (colorScheme === "" || colorScheme === null || !colorScheme) {
+            chrome.storage.sync.set({ "colorScheme": "midnight" }, () => {})
+            console.log("Default color scheme set to 'midnight'")
         }
+    })
+
+    if (changeInfo.status === "loading") {
+        chrome.storage.sync.get("colorScheme", ({colorScheme}) => {
+            if (tab.url.startsWith("https://www.vklass.se/")) {
+                chrome.scripting.insertCSS({
+                    target: {tabId: tabId},
+                    files: [`colorSchemes/${colorScheme}.css`]
+                }, () => {
+                    console.log("Inserted color scheme");
+                })
+                chrome.scripting.insertCSS({
+                    target: {tabId: tabId},
+                    files: ["css/main.css"]
+                }, () => {
+                    console.log("Inserted");
+                })
+            }
+        })
+
     }
 })
