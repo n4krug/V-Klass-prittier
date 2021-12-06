0a495f631518cb7831cf96bc9d11b0cf39d8c78b
n4krug <gustav.eneberg@gmail.com>
2021-12-06 22:19:56 +0100

    improved popup with intelligent color scheme and input

--- popup.js
+++ popup.js
@@ -1,6 +1,24 @@
 // const submit_btn = document.getElementById("submit-btn");
 
 let current_color_scheme = chrome.storage.sync.get("colorScheme")
+chrome.storage.sync.get(["colorScheme"], result => {
+    console.log(result)
+    
+    var cssId = 'myCss';  // you could encode the css path itself to generate id..
+    if (!document.getElementById(cssId))
+    {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = `colorSchemes/${result.colorScheme}.css`;
+        link.media = 'all';
+        head.appendChild(link);
+    }
+
+    color_scheme_inp.value = result.colorScheme
+})
 
 let color_scheme_inp = document.getElementById("color-scheme-select");
 
@@ -9,7 +27,7 @@ avaliableSchemes = ["none", "midnight", "midday", "darkness", "dark-mint", "dark
 // color_scheme_inp.selectedIndex = avaliableSchemes.indexOf(current_color_scheme)
 
 
-document.getElementById("submit-btn").addEventListener("click", () => {
+document.getElementById("color-scheme-select").addEventListener("change", () => {
     
     
     let color_scheme = color_scheme_inp.value;
@@ -19,5 +37,7 @@ document.getElementById("submit-btn").addEventListener("click", () => {
     color_scheme_inp.value = color_scheme
 
     chrome.tabs.reload()
+
+    location.reload()
 
 });
